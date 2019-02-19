7a15822f4533875b83da11e071ce4ca6eb4579a6
zengdewu <1315517612@qq.com>
2019-02-19 21:34:36 +0800

    dao修改

--- tensquare_base/src/main/java/com/tensquare/base/dao/LabelDao.java
+++ tensquare_base/src/main/java/com/tensquare/base/dao/LabelDao.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface LabelDao extends JpaRepository<Label,String>, JpaSpecificationExecutor<Label> {
     public void streamAllByCount();
+
 }
